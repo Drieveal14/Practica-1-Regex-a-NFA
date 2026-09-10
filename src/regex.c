6e970e5bb53a0c34643531327a8fead0c91e7aac
MaChavezM <marcoantonio.chavez594@ciencias.unam.mx>
2026-09-09 22:14:22 -0600

    Implementación de la funcion make explicit la cual ayuda a que dada una cadena se inserta el concat op

--- src/regex.c
+++ src/regex.c
@@ -45,7 +45,47 @@ static int ends_operand(char c)
  * El resultado es una copia que el llamador debe liberar con free(). */
 static char *make_explicit(const char *input)
 {
+    size_t len = strlen(input);
+    char *out = malloc((len * 2 + 1) * sizeof(char)); 
+    size_t j = 0;
 
+    int prev_ends_operand = 0; /* ¿el último token emitido puede cerrar un operando? */
+
+    for (size_t i = 0; i < len; i++)
+    {
+        char c = input[i];
+        int escaped = 0;
+        char literal = c;
+
+        if (c == '\\' && i + 1 < len)
+        {
+            escaped = 1;
+            i++;
+            literal = input[i];
+        }
+
+        char effective = escaped ? 'a' /* cualquier literal se comporta igual */ : c;
+
+        if (prev_ends_operand && starts_operand(effective) && effective != CONCAT_OP)
+        {
+            out[j++] = CONCAT_OP;
+        }
+
+        if (escaped)
+        {
+            out[j++] = '\\';
+            out[j++] = literal;
+            prev_ends_operand = 1;
+        }
+        else
+        {
+            out[j++] = c;
+            prev_ends_operand = ends_operand(c);
+        }
+    }
+
+    out[j] = '\0';
+    return out;
     
 }
 
